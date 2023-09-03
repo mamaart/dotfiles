74fed23dc4a2fe6706b544e13862bc883cfa39b3
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-03 13:58:31 +0200

    add cumbia radio

--- modules/radio/default.nix
+++ modules/radio/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}:
 
 {
-  systemd.user.services.radio = {
+  systemd.user.services.dr2 = {
     enable = true;
     description = "The radio as a background service";
     unitConfig = {
@@ -10,6 +10,15 @@
     serviceConfig = {
       ExecStart = "${pkgs.mplayer}/bin/mplayer -nocache -afm ffmpeg https://drliveradio1.akamaized.net/hls/live/2097651/p2/masterab.m3u8";
     };
-    #wantedBy = [ "multi-user.target" ];
+  };
+  systemd.user.services.cumbia = {
+    enable = true;
+    description = "The radio as a background service";
+    unitConfig = {
+      Type = "simple";
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.mplayer}/bin/mplayer -nocache http://streaming.hdserver.biz:9300/stream/1/";
+    };
   };
 }
