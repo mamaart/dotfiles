dbc2cc695c4401bb6a32dd47ff4bba6ada04d031
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-03 15:31:52 +0200

    add correct sha

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -27,7 +27,7 @@
             owner = "romkatv";
             repo = "powerlevel10k";
             rev = "v1.19.0";
-            sha256 = "0000000000000000000000000000000000000000000000000000";
+            hash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
           };
         }
       ];
