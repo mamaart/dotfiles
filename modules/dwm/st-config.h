8559573163e90880bb0cd45e6bfab72ab3c71eda
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-04 16:15:55 +0200

    use go font

--- modules/dwm/st-config.h
+++ modules/dwm/st-config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Go Mono Nerd Font:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
