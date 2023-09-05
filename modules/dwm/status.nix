0a9574fd96ec305c7baf3feab00ca056691997f1
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-05 02:33:20 +0200

    add some stuf

--- modules/dwm/status.nix
+++ modules/dwm/status.nix
@@ -1,16 +1,27 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   environment.systemPackages =
     let
       some-package = pkgs.buildGoModule {
-        vendorHash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
+        pname = "dwm-status";
+        version = "0.0.1";
+
         src = pkgs.fetchFromGitHub {
           owner = "mamaart";
           repo = "dwm-status";
           rev = "v0.0.1";
           hash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
         };
+
+        vendorHash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
+
+        #meta = with lib; {
+        #  description = "DWM status bar with todo list";
+        #  homepage = "https://github.com/mamaart/dwm-status";
+        #  licence = licenses.gpl3;
+
+        #};
       };
     in
     [ some-package ];
