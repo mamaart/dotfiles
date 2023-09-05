b906845125d98d4dc2ceb0d28adb12c6d4beb2b8
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-05 07:00:17 +0200

    try to merge

--- modules/dwm/default.nix
+++ modules/dwm/default.nix
@@ -7,20 +7,20 @@ let
     src = builtins.fetchGit {
       url = "https://github.com/mamaart/dwm-status.git";
       ref = "main";
-      rev = "e5483969104389d110c47e2f033bfb80c66ab77a";
+      rev = "dafc1fb792224aa620b5349369f59e3bbddfc089";
     };
     vendorHash = "sha256-bZ8BbYgebatTQh4KVv2J0hBLwPuOHZaQAQX3o63R4HU=";
   };
-  dwm-statusctl = pkgs.buildGoModule {
-    pname = "statusbarctl";
-    version = "0.0.3";
-    src = builtins.fetchGit {
-      url = "https://github.com/mamaart/dwm-statusctl.git";
-      ref = "main";
-      rev = "240e60e509a2361be542e481b46fe8b6bc630378";
-    };
-    vendorHash = "sha256-JFvC9V0xS8SZSdLsOtpyTrFzXjYAOaPQaJHdcnJzK3s=";
-  };
+  #dwm-statusctl = pkgs.buildGoModule {
+  #  pname = "statusbarctl";
+  #  version = "0.0.3";
+  #  src = builtins.fetchGit {
+  #    url = "https://github.com/mamaart/dwm-statusctl.git";
+  #    ref = "main";
+  #    rev = "240e60e509a2361be542e481b46fe8b6bc630378";
+  #  };
+  #  vendorHash = "sha256-JFvC9V0xS8SZSdLsOtpyTrFzXjYAOaPQaJHdcnJzK3s=";
+  #};
 in {
   services.xserver = {
     enable = true;
@@ -44,7 +44,7 @@ in {
 
   environment = {
     systemPackages = [
-        dwm-statusctl
+        #dwm-statusctl
         dwm-status
         pkgs.dmenu
         pkgs.arandr
