98420ec3a0b0133cfb67f799d7d1d50f99739508
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-05 02:46:29 +0200

    update version

--- modules/dwm/status.nix
+++ modules/dwm/status.nix
@@ -12,11 +12,11 @@
         src = pkgs.fetchFromGitHub {
           owner = "mamaart";
           repo = "dwm-status";
-          rev = "v0.0.1";
-          hash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
+          rev = "v0.0.2";
+          sha256 = lib.fakeSha256;
         };
 
-        vendorHash = "sha256-+hzjSbbrXr0w1rGHm6m2oZ6pfmD6UUDBfPd7uMg5l5c=";
+        vendorHash = lib.fakeSha256;
 
         #meta = with lib; {
         #  description = "DWM status bar with todo list";
