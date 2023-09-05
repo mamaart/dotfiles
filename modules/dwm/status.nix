bccf35f5993f6699eedead64049b63d7d2e0ff6e
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-05 02:58:54 +0200

    refactor

--- modules/dwm/status.nix
+++ modules/dwm/status.nix
@@ -1,30 +1,17 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  environment.systemPackages =
-    let
-      some-package = pkgs.buildGoModule {
-        pname = "dwm-status";
-        version = "0.0.1";
-
-        #goPackagePath = "github.com/mamaart/dwm-status";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "mamaart";
-          repo = "dwm-status";
-          rev = "v0.0.2";
-          hash = "sha256-7xkC8HM+Gwee+Yo79kpEYx6tz7r5msWHNh7suRdF/ck=";
-        };
-
-        vendorHash = "sha256-bZ8BbYgebatTQh4KVv2J0hBLwPuOHZaQAQX3o63R4HU=";
-
-        #meta = with lib; {
-        #  description = "DWM status bar with todo list";
-        #  homepage = "https://github.com/mamaart/dwm-status";
-        #  licence = licenses.gpl3;
-
-        #};
+  environment.systemPackages = [
+    pkgs.buildGoModule {
+      pname = "dwm-status";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "mamaart";
+        repo = "dwm-status";
+        rev = "v0.0.2";
+        hash = "sha256-7xkC8HM+Gwee+Yo79kpEYx6tz7r5msWHNh7suRdF/ck=";
       };
-    in
-    [ some-package ];
+      vendorHash = "sha256-bZ8BbYgebatTQh4KVv2J0hBLwPuOHZaQAQX3o63R4HU=";
+    }
+  ];
 }
