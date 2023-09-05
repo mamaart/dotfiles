88cd0f605750afe6ba4b01c6b19f8dc624cac94f
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-05 03:13:14 +0200

    add ctl

--- modules/dwm/status.nix
+++ modules/dwm/status.nix
@@ -4,11 +4,22 @@
   environment.systemPackages = [
     pkgs.buildGoModule {
       pname = "dwm-status";
-      version = "0.0.1";
+      version = "0.0.2";
       src = pkgs.fetchFromGitHub {
         owner = "mamaart";
         repo = "dwm-status";
         rev = "v0.0.2";
+        hash = "sha256-7xkC8HM+Gwee+Yo79kpEYx6tz7r5msWHNh7suRdF/ck=";
+      };
+      vendorHash = "sha256-bZ8BbYgebatTQh4KVv2J0hBLwPuOHZaQAQX3o63R4HU=";
+    }
+    pkgs.buildGoModule {
+      pname = "dwm-status-ctl";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "mamaart";
+        repo = "dwm-status-ctl";
+        rev = "v0.0.1";
         hash = "sha256-7xkC8HM+Gwee+Yo79kpEYx6tz7r5msWHNh7suRdF/ck=";
       };
       vendorHash = "sha256-bZ8BbYgebatTQh4KVv2J0hBLwPuOHZaQAQX3o63R4HU=";
