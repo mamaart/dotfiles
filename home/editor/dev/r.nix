58dbaaad470b61e3120e64b61f0921c7970758ba
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-09 22:29:19 +0200

    use latest comit

--- home/editor/dev/r.nix
+++ home/editor/dev/r.nix
@@ -4,8 +4,8 @@ let nvim-r = pkgs.vimUtils.buildVimPlugin {
     name = "nvim-r";
     src = fetchgit {
       url= "https://github.com/jalvesaq/nvim-r";
-      rev =  "c53b5a402a26df5952718f483c7461af5bb459eb";
-      sha256 = "13xbb05gnpgmyaww6029saplzjq7cq2dxzlxylcynxhhyibz5ibv";
+      rev =  "master";
+      ref = "d8f7588fda8d06e8005252178c872e169e96ce1c";
       };
     buildInputs = [ which vim  zip];
 };
@@ -16,7 +16,6 @@ in
     rPackages.plotly
     rPackages.txtplot
     rPackages.languageserver
-    #(import ./vim.nix)
     vimPlugins.coc-r-lsp
     vimPlugins.nvim-treesitter-parsers.r
     ((vim_configurable.override { }).customize {
