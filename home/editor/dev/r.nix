7f2c8ff52a4b07f569a0362d56e0744f2cf8e5f1
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-09 21:25:05 +0200

    fix

--- home/editor/dev/r.nix
+++ home/editor/dev/r.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }:
+with pkgs;
+let customPlugins = {
+  nvim-r = vimUtils.buildVimPlugin {
+    name = "nvim-r";
+    src = fetchgit {
+      url= "https://github.com/jalvesaq/nvim-r";
+      rev =  "c53b5a402a26df5952718f483c7461af5bb459eb";
+      sha256 = "13xbb05gnpgmyaww6029saplzjq7cq2dxzlxylcynxhhyibz5ibv";
+      };
+    buildInputs = [ which vim  zip];
+  };
+};
+in
 {
   home.packages = with pkgs; [
     R
@@ -10,8 +23,8 @@
     vimPlugins.nvim-treesitter-parsers.r
     ((vim_configurable.override { }).customize {
       name = "vim";
-      vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
-      vimrcConfig.vam.pluginDictionaries = [
+      vimrcConfig.packages.knownPlugins = pkgs.vimPlugins // customPlugins;
+      vimrcConfig.packages.pluginDictionaries = [
         { names = [
           "nvim-r"
         ]; }
