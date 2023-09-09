4263f3271804638402f41c3e4a257a0a581cd7c5
Martin Maartensson <martinmaartensson@gmail.com>
2023-09-09 21:23:20 +0200

    fix

--- home/editor/dev/r.nix
+++ home/editor/dev/r.nix
@@ -5,17 +5,17 @@
     rPackages.plotly
     rPackages.txtplot
     rPackages.languageserver
-    (import ./vim.nix)
+    #(import ./vim.nix)
     vimPlugins.coc-r-lsp
     vimPlugins.nvim-treesitter-parsers.r
-    #((vim_configurable.override { }).customize {
-    #  name = "vim";
-    #  vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
-    #  vimrcConfig.vam.pluginDictionaries = [
-    #    { names = [
-    #      "nvim-r"
-    #    ]; }
-    #  ]; 
-    #})
+    ((vim_configurable.override { }).customize {
+      name = "vim";
+      vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
+      vimrcConfig.vam.pluginDictionaries = [
+        { names = [
+          "nvim-r"
+        ]; }
+      ]; 
+    })
   ];
 }
