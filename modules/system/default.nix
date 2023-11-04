0fe643a203652bb9a7efd4a9661ee1ff92267cf5
Martin Maartensson <martinmaartensson@gmail.com>
2023-11-04 01:35:13 +0100

    edit

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -45,17 +45,5 @@
 
   services.tor = {
     enable = true;
-    openFirewall = true;
-    relay = {
-      enable = true;
-      role = "relay";
-    };
-    settings = {
-      ContactInfo = "toradmin@example.org";
-      Nickname = "toradmin";
-      ORPort = 9001;
-      ControlPort = 9051;
-      BandWidthRate = "1 MBytes";
-    };
   };
 }
